f96698a3bea6949facc184f89143c72fecdbe4a4
Omega <omega@alpha.org>
2015-01-19 19:44:06 -0500

    First commit

---
+++ IrcBot.scala
@@ -0,0 +1,47 @@
+import java.net._
+import java.io._
+import scala.io._
+
+object IrcBot {
+  def main(args : Array[String]) {
+	var nick: String = "NICK ScalaBot\r\n";
+	var user: String = "USER ScalaBot ScalaBot ScalaBot :ScalaBot\r\n";
+	var join: String = "JOIN #mootsinsuits\r\n";
+    val socket = new Socket(InetAddress.getByName("irc.tm"), 6667)
+    var in = new BufferedSource(socket.getInputStream).getLines
+    val out = new PrintStream(socket.getOutputStream)
+	while(true){
+		var data: String = in.next;
+		println(data);
+		if(data contains "001"){
+			out.println(join);
+			out.flush();
+		}
+		if(data contains "Found your hostname"){
+			out.println(user);
+			out.flush;
+			out.println(nick);
+			out.flush();	
+		}
+		if(data contains "PING :"){
+			if(data contains "You have not registered"){
+			}
+			else{
+				var hash_array: Array[String] = data.split("PING :");
+				println("Sending " + "PONG :"+hash_array(1));
+				out.println("PONG :"+hash_array(1)+"\r\n");
+				out.flush();
+			}
+		 }
+		if(data contains ".quit"){
+			out.println("QUIT I'm gay\r\n");
+			out.flush();
+			socket.close;
+		 }
+		if(data contains ".scala"){
+			out.println("PRIVMSG #mootsinsuits :I am a bot made in scala");
+			out.flush();
+		}
+	  }
+   }  
+}
